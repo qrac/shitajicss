0ccdad0040ba3cc1184f7c46a10cdfd5ac7007d4
Qrac <qrac.jp@gmail.com>
2022-05-14 00:56:12 +0900

    Upgrade development

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-//----------------------------------------------------
-// Variables
-//----------------------------------------------------
-
-const fs = require("fs")
-const CleanCSS = require("clean-css")
+import fs from "fs"
+import CleanCSS from "clean-css"
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"))
 const pjt = JSON.parse(fs.readFileSync("./project.json", "utf8"))
@@ -11,29 +7,22 @@ const tmp = fs.readFileSync("./src/index.css", "utf8")
 
 const distCssDir = "./dist"
 
-//----------------------------------------------------
-// Functions
-//----------------------------------------------------
-
 const createDir = (dir) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true })
   }
 }
 
-//----------------------------------------------------
-// Actions
-//----------------------------------------------------
-
 createDir(distCssDir)
 
 const distCss = `${distCssDir}/${pjt.fileName}.css`
 const distCssMin = `${distCssDir}/${pjt.fileName}.min.css`
 
-data = tmp.replace(/___pjtName___/g, pjt.name)
-data = data.replace(/___pkgVersion___/g, pkg.version)
-data = data.replace(/___pkgLicense___/g, pkg.license)
-data = data.replace(/___pkgAuthorName___/g, pkg.author.name)
+const data = tmp
+  .replace(/___pjtName___/g, pjt.name)
+  .replace(/___pkgVersion___/g, pkg.version)
+  .replace(/___pkgLicense___/g, pkg.license)
+  .replace(/___pkgAuthorName___/g, pkg.author.name)
 
 const resultCss = data
 const resultCssMin = new CleanCSS({}).minify(data)
